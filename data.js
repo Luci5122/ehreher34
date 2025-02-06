abe5520914b3fe4a57ff7113a2643a951be61a60
Luci5122 <bravera@tuta.io>
2025-02-06 19:14:58 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "YOUR_TELEGRAM_BOT_TOKEN",
-  id:"Enter Your Id",
+  token: "7152197918:AAEk5hlrG_J4hv73QRqoJ1QpyJR7Xsg5JkM",
+  id:"7997421833",
   address:"Enter your url"
 };
